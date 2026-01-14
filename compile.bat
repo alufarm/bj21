53317a1b39475d4077d75174bf5cd6c43cf3e2ff
alufarm <wornikarn@gmail.com>
2026-01-14 14:40:09 +0300

    initial commit

---
+++ compile.bat
@@ -0,0 +1,1 @@
+cmd.exe /k "g++ -std=c++17 source.cpp -o source.exe -ISFML-2.5.1/include -LSFML-2.5.1/lib -lsfml-graphics -lsfml-window -lsfml-system && source.exe
---
+++ source.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+struct Timer{
+	Timer(): time(std::chrono::high_resolution_clock::now()){}
+	
+	double elapsed(){
+		double elapsed = (std::chrono::high_resolution_clock::now() - time).count();
+		time = std::chrono::high_resolution_clock::now();
+		return elapsed / 1e9;
+	}
+	
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> time;
+};
+
+template<typename T>
+void print(T value){
+	std::cout << value << std::endl;
+}
+
+void print(sf::FloatRect fRect){
+	print(std::to_string(fRect.left) + ", " + std::to_string(fRect.top) + ", " + std::to_string(fRect.width) + ", " + std::to_string(fRect.height));
+}
+
+void print(sf::Vector2i iVect){
+	print(std::to_string(iVect.x) + ", " + std::to_string(iVect.y));
+}
+
+bool checkCollision(sf::FloatRect fRect, sf::Vector2i point){
+	if(point.x > fRect.left && point.x < fRect.left + fRect.width
+		&& point.y > fRect.top && point.y < fRect.top + fRect.height){
+		return true;
+	}
+	return false;
+}
+
+
+float interpolate(float a, float b, float p){
+	return a + (b-a) * p;
+}
+
+struct RectShape{
+	
+	void SetPosition(sf::Vector2f position){
+		shape.setPosition(position);
+		collision = shape.getGlobalBounds();
+	}
+	
+	void SetSize(sf::Vector2f size){
+		shape.setSize(size);
+	}
+	
+	sf::FloatRect GetGlobalBounds(){
+		return shape.getGlobalBounds();
+	}
+	
+	bool CheckCollision(sf::Vector2i point){
+		if(point.x > collision.left && point.x < collision.left + collision.width
+			&& point.y > collision.top && point.y < collision.top + collision.height){
+			return true;
+		}
+		return false;
+	}
+	
+private:
+	sf::RectangleShape shape;
+	sf::FloatRect collision;
+	bool onCollision;
+};
+
+struct Image{
+	sf::Image image;
+	sf::Texture texture;
+	sf::Sprite sprite;
+	
+	void Load(std::string path){
+		if (!(image.loadFromFile(path)))
+			std::cout << "Cannot load image";
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);  
+	}
+};
+
+int main(){
+	int width = 800, height = 600;
+	
+	sf::RenderWindow window(sf::VideoMode(width, height), "21");
+	
+	sf::Vector2i mouseMove;
+	
+	sf::Vector2f cardSize(110, 180);
+	sf::RectangleShape card(cardSize);
+	card.setPosition(width / 2 - cardSize.x / 2, height / 2 - cardSize.y / 2);
+	card.setSize(cardSize);
+	bool cardEnter = false;
+	sf::FloatRect cardCollision = card.getGlobalBounds();
+	
+	float riseAmount = 50;
+	
+	double targetDurationTime = 1.0;
+	double durationAccumTime = 0;
+	int frames = 0;
+	
+	double deltaTime = 0;
+	
+	Timer frameTimer;
+	
+	sf::Event event;
+	while(window.isOpen()){
+		deltaTime = frameTimer.elapsed();
+		
+		frames++;
+		durationAccumTime += deltaTime;
+		if(durationAccumTime > targetDurationTime){
+			//print(frames);
+			frames = 0;
+			durationAccumTime = 0;
+		}
+
+		
+		while(window.pollEvent(event)){
+			
+			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::Resized){
+				width = event.size.width;
+				height = event.size.height;
+				
+				sf::FloatRect visibleArea(0.f, 0.f, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
+				
+				card.setPosition(width / 2 - cardSize.x / 2, height / 2 - cardSize.y / 2);
+				card.setSize(cardSize);
+				
+				if(cardEnter){
+					cardCollision = card.getGlobalBounds();
+					cardCollision.height+=50;
+				}
+				else
+					cardCollision = card.getGlobalBounds();
+			}
+			
+			if(event.type == sf::Event::MouseMoved){
+				mouseMove.x = event.mouseMove.x;
+				mouseMove.y = event.mouseMove.y;
+			}
+			
+		}
+
+		window.clear();		
+		
+		if(checkCollision(cardCollision, mouseMove)){
+			if(cardEnter != true){
+				cardEnter = true;
+				card.setFillColor(sf::Color(0, 120, 255));
+				
+				card.setPosition(card.getPosition().x, card.getPosition().y - riseAmount);
+				cardCollision = card.getGlobalBounds();
+				cardCollision.height+=50;
+	
+			}
+		}
+		else{
+			if(cardEnter != false){
+				cardEnter = false;
+				card.setFillColor(sf::Color(255, 255, 255));
+				
+				card.setPosition(width / 2 - cardSize.x / 2, height / 2 - cardSize.y / 2);
+				cardCollision = card.getGlobalBounds();
+			}
+		}
+			
+		window.draw(card);
+		
+		window.display();
+	}
+	
+	return 0;
+}
